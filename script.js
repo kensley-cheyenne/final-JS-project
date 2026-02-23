e928c2d70eb680d6f6b8312b6596a5bc95125755
kensley-cheyenne <mkensley07@gmail.com>
2026-02-23 13:20:07 -0600

    finished

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let movies = [];
-const searchInput = document.querySelector("#searchInput"); 
-// sort //
+let movies;
+const moviesWrapper = document.querySelector(".movies");
+const searchInput = document.querySelector("#searchInput");
+
 function filterMovies(event) {
   const filter = event.target.value;
-    if (filter === "NEW_TO_OLD") {
+
+  console.log(movies);
+
+  if (filter === "NEW_TO_OLD") {
     movies = movies.sort(
       (a, b) => parseInt(b.Year.slice(0, 4)) - parseInt(a.Year.slice(0, 4))
     );
@@ -12,66 +16,43 @@ function filterMovies(event) {
       (a, b) => parseInt(a.Year.slice(0, 4)) - parseInt(b.Year.slice(0, 4))
     );
   }
+
   renderMovies(movies);
 }
-// search bar //
+
 function searchKeyword(event) {
-  searchInput.addEventListener("keydown", function(e) {
-    if (e.key === "Enter") {
-      searchKeyword({ target: searchInput });
-    }
-  });
+  getMovies(event.target.value);
+}
 
-  searchInput.addEventListener("input", function(e) {
-    if (!e.target.value) {
-      renderMovies(movies);
-    }
-  });
-
-  const keyword = event.target.value.toLowerCase();
-  const filtered = movies.filter(movie =>
-    movie.Title.toLowerCase().includes(keyword)
+async function getMovies(searchValue) {
+  const response = await fetch(
+    `https://www.omdbapi.com/?apikey=d5c82cd3&s=${
+      searchValue || "All"
+    }`
   );
-  renderMovies(filtered);
+  const { Search: data } = await response.json();
+  movies = data;
+  renderMovies(movies);
 }
 
-const searchBtn = document.querySelector(".fa-search");
-if (searchBtn) {
-  searchBtn.addEventListener("click", function() {
-    searchKeyword({ target: searchInput });
-  });
-}
-
-const moviesWrapper = document.querySelector(".movies");
 function renderMovies(movies) {
-  if (!movies || movies.length === 0) {
-    moviesWrapper.innerHTML = '<div class="no-matches">No matches</div>';
-    return;
-  }
-  const moviesHTMLString = movies.map((movie) => moviesHTML(movie)).join("");
-  moviesWrapper.innerHTML = moviesHTMLString;
+  const moviesHtml = movies.map((movie) => moviesHTML(movie)).join("");
+
+  moviesWrapper.innerHTML = moviesHtml;
 }
-// movies from API //
+
 function moviesHTML(movie) {
   return `<div class="movie">
-            <figure class="movie__img__wrapper">
-              <img class="movie__img" src="${movie.Poster}" alt="">
-            </figure>
-            <div class="movie__title">
-              ${movie.Title}
-            </div>
-            <div class="movie__year">
-              ${movie.Year}
-            </div>
-          </div>`;
+  <figure class="movie__img__wrapper">
+    <img class="movie__img" src="${movie.Poster}" alt="">
+  </figure>
+  <div class="movie__title">
+    ${movie.Title}
+  </div>
+  <div class="movie__year">
+    ${movie.Year}
+  </div>
+</div>`;
 }
 
-async function getMovies(searchInput) {
-  const response = await fetch(
-    `https://www.omdbapi.com/?i=tt3896198&apikey=d5c82cd3&s=${searchInput || "all"}`
-  );
-  const { Search : data } = await response.json();
-  movies = data
-  renderMovies(movies);
-}
 getMovies();
